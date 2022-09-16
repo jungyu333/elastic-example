a827b8471ef283f6c0e62d795a476b527db59f7a
jungyu <jungyu@naver.com>
2022-09-16 14:37:43 +0900

    main page example search-bar 구현

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 2rem;
+`;
+
+const SearchForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+  & input {
+    padding: 10px;
+    margin-right: 10px;
+  }
+
+  & button {
+    padding: 10px;
+    background-color: white;
+    cursor: pointer;
+    :hover {
+      background-color: gray;
+    }
+  }
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const [search, setSearch] = useState('');
+
+  const onChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Title>Main Page</Title>
+      <SearchForm onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="검색하기"
+          value={search}
+          onChange={onChange}
+        />
+        <button type="submit">검색</button>
+      </SearchForm>
+    </>
+  );
 }
 
 export default Home;
